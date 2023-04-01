96089b33c934adf0e89f5eced6c518a3cafc3410
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-03-31 23:14:09 -0700

    feat: add simple server

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-console.log("Hello World");
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+const router = new Router();
+
+type Context = {
+  response: {
+    body: string;
+  };
+};
+
+router.get("/", (stx: Context) => {
+  stx.response.body = "Hello World!";
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
