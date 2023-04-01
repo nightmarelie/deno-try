c7e86f34eafd4fa44580dfbca663d66b8769b786
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-03-31 23:23:04 -0700

    feat: add about router and 404

--- main.ts
+++ main.ts
@@ -3,17 +3,21 @@ import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 const router = new Router();
 
-type Context = {
-  response: {
-    body: string;
-  };
-};
+router.get("/", (ctx) => {
+  ctx.response.body = "Hello World!";
+});
 
-router.get("/", (stx: Context) => {
-  stx.response.body = "Hello World!";
+router.get("/about", (ctx) => {
+  ctx.response.body = "About Page";
 });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+// default 404 page
+app.use((ctx) => {
+  ctx.response.body = "Page not found";
+  ctx.response.status = 404;
+});
 
 await app.listen({ port: 8000 });
